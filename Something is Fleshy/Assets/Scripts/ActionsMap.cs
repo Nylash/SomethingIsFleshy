57908fa2b7e36858285eaf162cb41fcd9ac29f31
Ben <ben.peltier7@gmail.com>
2020-05-26 16:42:52 +0200

    Basic movement

---
+++ Something is Fleshy/Assets/Scripts/ActionsMap.cs
@@ -0,0 +1,194 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/ActionsMap.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @ActionsMap : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @ActionsMap()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""ActionsMap"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""9f7e5fce-4e55-46bf-8ebe-ecb1055b294d"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""bc0fa8a4-1316-46d1-9b25-9e277604de50"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a8bec3f9-fc33-4479-a459-dd47ff97d02b"",
+                    ""path"": ""<Gamepad>/leftStick/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""b31eb623-4119-41fa-b623-1623c4b98c8b"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ce494b00-f31a-4cc7-954a-f3e95b4372b3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""b05fa505-88bc-4e17-8d1e-81f660644aa1"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": []
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": []
+        }
+    ]
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Movement = m_Gameplay.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Movement;
+    public struct GameplayActions
+    {
+        private @ActionsMap m_Wrapper;
+        public GameplayActions(@ActionsMap wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Gameplay_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ Something is Fleshy/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+#pragma warning disable 0649
+	[Header("CONFIGURATION")]
+	[SerializeField] private float movementSpeed = 10f;
+	[Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+	[SerializeField] private bool airControl = false;
+	[SerializeField] private float airControlSpeed = 5f;
+	[SerializeField] private LayerMask whatIsGround;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private float groundedRadius = .2f;
+#pragma warning restore 0649
+
+	[Header("COMPONENTS")]
+	public Rigidbody2D rb;
+	ActionsMap actionsMap;
+
+	[Header("VARIABLES")]
+	public bool grounded;
+	bool facingRight = true;
+	Vector3 velocity = Vector3.zero;
+	float movementInput;
+
+	private void OnEnable() => actionsMap.Gameplay.Enable();
+	private void OnDisable() => actionsMap.Gameplay.Disable();
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		actionsMap = new ActionsMap();
+
+		actionsMap.Gameplay.Movement.performed += ctx => movementInput = ctx.ReadValue<float>();
+		actionsMap.Gameplay.Movement.canceled += ctx => movementInput = 0f;
+	}
+
+	private void FixedUpdate()
+	{
+		bool wasGrounded = grounded;
+		grounded = false;
+
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				grounded = true;
+				if (!wasGrounded)
+				{
+					//Landing
+				}
+			}
+		}
+		if (!grounded)
+		{
+			//inAir
+		}
+		Move(movementInput);
+	}
+
+	public void Move(float horizontalMove)
+	{
+		if (grounded)
+		{
+			Vector3 targetVelocity = new Vector2(horizontalMove * movementSpeed, rb.velocity.y);
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+			if (horizontalMove > 0 && !facingRight)
+				Flip();
+			else if (horizontalMove < 0 && facingRight)
+				Flip();
+		}
+		else if (airControl)
+		{
+			Vector3 targetVelocity = new Vector2(horizontalMove * airControlSpeed, rb.velocity.y);
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+			if (horizontalMove > 0 && !facingRight)
+				Flip();
+			else if (horizontalMove < 0 && facingRight)
+				Flip();
+		}
+	}
+
+	private void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(groundCheck.position, groundedRadius);
+	}
+}
