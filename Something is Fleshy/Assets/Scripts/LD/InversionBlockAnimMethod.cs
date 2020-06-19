7a06364016edad1c1b68dc8883750d7466798751
Ben <ben.peltier7@gmail.com>
2020-06-19 17:55:50 +0200

    Inversion Blocks

---
+++ Something is Fleshy/Assets/Scripts/LD/InversionBlockAnimMethod.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+public class InversionBlockAnimMethod : MonoBehaviour
+{
+    PolygonCollider2D coll;
+    SpriteShapeRenderer shapeRenderer;
+
+    private void Start()
+    {
+        coll = GetComponent<PolygonCollider2D>();
+        shapeRenderer = GetComponent<SpriteShapeRenderer>();
+    }
+
+    void ActivateCollider()
+    {
+        coll.enabled = true;
+    }
+
+    void ActivateRenderer()
+    {
+        shapeRenderer.enabled = true;
+    }
+
+    void DeactivateColliderAndRenderer()
+    {
+        shapeRenderer.enabled = false;
+        coll.enabled = false;
+    }
+}
---
+++ Something is Fleshy/Assets/Scripts/LD/InversionBlocks.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class InversionBlocks : MonoBehaviour
+{
+    #region CONFIGURATION
+#pragma warning disable 0649
+    [Header("PARAMETERS")]
+    [Tooltip("Time before the first activity.")]
+    [SerializeField] float timeBetweenSwitch = 10f;
+    [Tooltip("Check this if you want blocksB to be the first activated.")]
+    [SerializeField] bool startWithBlocksB;
+#pragma warning restore 0649
+    #endregion
+
+    [Header("Variables")]
+    [Header("⚠ DON'T TOUCH BELOW ⚠")]
+    public Animator[] blocksA;
+    public Animator[] blocksB;
+    bool blockAactivated = true;
+
+    private void Start()
+    {
+        int nbAblocks = transform.GetChild(0).transform.childCount;
+        blocksA = new Animator[nbAblocks];
+        for (int i = 0; i < nbAblocks; i++)
+        {
+            blocksA[i] = transform.GetChild(0).transform.GetChild(i).GetComponent<Animator>();
+        }
+        int nbBblocks = transform.GetChild(1).transform.childCount;
+        blocksB = new Animator[nbBblocks];
+        for (int i = 0; i < nbBblocks; i++)
+        {
+            blocksB[i] = transform.GetChild(1).transform.GetChild(i).GetComponent<Animator>();
+        }
+        if (!startWithBlocksB)
+        {
+            foreach (Animator item in blocksB)
+                item.SetTrigger("Depop");
+        }
+        else
+        {
+            blockAactivated = false;
+            foreach (Animator item in blocksA)
+                item.SetTrigger("Depop");
+        }
+        InvokeRepeating("InverseBlocks", timeBetweenSwitch, timeBetweenSwitch);
+    }
+
+    void InverseBlocks()
+    {
+        if (blockAactivated)
+        {
+            blockAactivated = false;
+            foreach (Animator item in blocksA)
+                item.SetTrigger("Depop");
+            foreach (Animator item in blocksB)
+                item.SetTrigger("Pop");
+        }
+        else
+        {
+            blockAactivated = true;
+            foreach (Animator item in blocksA)
+                item.SetTrigger("Pop");
+            foreach (Animator item in blocksB)
+                item.SetTrigger("Depop");
+        }
+    }
+}
