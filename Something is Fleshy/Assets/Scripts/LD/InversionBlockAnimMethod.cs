d28f442c3ccdde43a59aceea441272c91f86e4e1
Ben <ben.peltier7@gmail.com>
2020-07-29 18:55:15 +0200

    Inverse blocks security

--- Something is Fleshy/Assets/Scripts/LD/InversionBlockAnimMethod.cs
+++ Something is Fleshy/Assets/Scripts/LD/InversionBlockAnimMethod.cs
@@ -5,15 +5,19 @@ public class InversionBlockAnimMethod : MonoBehaviour
 {
     PolygonCollider2D coll;
     SpriteShapeRenderer shapeRenderer;
+    Animator anim;
 
     private void Start()
     {
         coll = GetComponent<PolygonCollider2D>();
         shapeRenderer = GetComponent<SpriteShapeRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void ActivateCollider()
     {
+        anim.SetBool("PlayerIn", false);
+        coll.isTrigger = false;
         coll.enabled = true;
     }
 
@@ -26,5 +30,30 @@ public class InversionBlockAnimMethod : MonoBehaviour
     {
         shapeRenderer.enabled = false;
         coll.enabled = false;
+        coll.isTrigger = false;
+        anim.SetBool("PlayerIn", false);
+    }
+
+    private void Update()
+    {
+        if (coll.isActiveAndEnabled)
+        {
+            if (coll.bounds.Contains(CharacterController2D.instance.transform.position))
+            {
+                if (!coll.isTrigger)
+                {
+                    coll.isTrigger = true;
+                    anim.SetBool("PlayerIn", true);
+                }
+            }
+            else
+            {
+                if (coll.isTrigger)
+                {
+                    coll.isTrigger = false;
+                    anim.SetBool("PlayerIn", false);
+                }
+            } 
+        }
     }
 }
