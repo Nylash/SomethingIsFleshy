1054db77d6848050569b8431611a547e33d70198
Ben <ben.peltier7@gmail.com>
2020-08-08 15:07:26 +0200

    Leak mask

---
+++ Something is Fleshy/Assets/Scripts/Leak/LeakMask.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.U2D;
+using UnityEngine;
+
+public class LeakMask : MonoBehaviour
+{
+    SpriteShapeController shape;
+    float initialSize;
+
+    void Start()
+    {
+        shape = transform.parent.transform.parent.GetComponent<SpriteShapeController>();
+        initialSize = transform.localScale.y;
+    }
+
+    void Update()
+    {
+        if(shape.spriteShape == GameManager.instance.pipeOpenShape)
+        {
+            if (transform.localScale.y != initialSize)
+                transform.localScale = new Vector3(transform.localScale.x, initialSize, transform.localScale.z);
+        }
+        else
+        {
+            if (transform.localScale.y == initialSize)
+                transform.localScale = new Vector3(transform.localScale.x, initialSize / 1.65f, transform.localScale.z);
+        }
+    }
+}
