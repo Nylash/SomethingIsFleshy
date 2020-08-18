0c808756dc7085fe8390584897d90a5960be91ea
Ben <ben.peltier7@gmail.com>
2020-08-18 18:26:51 +0200

    New TP graphics

--- Something is Fleshy/Assets/Scripts/LD/Teleporters.cs
+++ Something is Fleshy/Assets/Scripts/LD/Teleporters.cs
@@ -13,13 +13,33 @@ public class Teleporters : MonoBehaviour
     [Header("Variables")]
     [Header("⚠ DON'T TOUCH BELOW ⚠")]
     public GameObject[] TPS = new GameObject[2];
+    Gradient fxColor;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
 
     private void Start()
     {
+        fxColor = new Gradient();
+        colorKey = new GradientColorKey[2];
+        colorKey[0].color = TPS_Color;
+        colorKey[0].time = 0.0f;
+        colorKey[1].color = Color.white;
+        colorKey[1].time = 1.0f;
+        alphaKey = new GradientAlphaKey[2];
+        alphaKey[0].alpha = 1.0f;
+        alphaKey[0].time = 0.0f;
+        alphaKey[1].alpha = 1.0f;
+        alphaKey[1].time = 1.0f;
+        fxColor.SetKeys(colorKey, alphaKey);
+
         TPS[0] = transform.GetChild(0).gameObject;
         TPS[1] = transform.GetChild(1).gameObject;
         foreach (GameObject item in TPS)
-            item.GetComponent<SpriteRenderer>().color = TPS_Color;
+        {
+            item.transform.GetChild(0).GetComponent<SpriteRenderer>().color = TPS_Color;
+            ParticleSystem.MainModule fx = item.transform.GetChild(1).GetComponent<ParticleSystem>().main;
+            fx.startColor = fxColor;
+        }  
     }
 
     public Vector3 GetTPLocation(GameObject currentTP)
